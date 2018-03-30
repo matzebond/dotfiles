4b62efff10dc2c0bee40577b1f8fd405baf6cded
Matthias Schmitt <male.schmitt@posteo.de>
2018-03-30 21:10:38 +0200

    Multi monitor polybar

--- i3/.config/polybar/launch.sh
+++ i3/.config/polybar/launch.sh
@@ -5,7 +5,10 @@ killall -q polybar
 while pgrep -x polybar >/dev/null; do sleep 1; done
 
 # Launch bar1 and bar2
-polybar main #&
-# polybar bar2 &
+DISPLAY1="$(xrandr -q | grep " connected" | grep 'eDP1\|HDMI-0' | cut -d ' ' -f1)"
+[[ ! -z "$DISPLAY1" ]] && MONITOR=$DISPLAY1 polybar main &
+
+DISPLAY2="$(xrandr -q | grep 'DVI-I-2\|VGA0' | cut -d ' ' -f1)"
+[[ ! -z $DISPLAY2 ]] && MONITOR=$DISPLAY2 polybar main &
 
 echo "Bars launched..."
