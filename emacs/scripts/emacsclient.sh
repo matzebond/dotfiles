d9061e9f5476a32ecb1358cca1d0fa42e9ce961a
Matthias Schmitt <matthias.schmitt@deepl.com>
2024-05-14 19:49:34 +0200

    [emacs] update emacsclient scripts

--- emacs/scripts/emacsclient.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-emacsclient -n -e "(if (> (length (frame-list)) 1) 't)" | grep t
-if [ "$?" = "1" ]; then
-    emacsclient -c -n "$@"
-else
-    emacsclient -n "$@"
-fi
--- emacs/scripts/emacsmail.sh
+++ emacs/scripts/emacsmail.sh
@@ -1,8 +1,3 @@
 #!/bin/sh
 
-emacsclient -n -e "(if (> (length (frame-list)) 1) 't)" | grep t
-if [ "$?" = "1" ]; then
-    emacsclient -c -n --eval "(progn (x-focus-frame nil)(browse-url-mail \"$@\"))"
-else
-    emacsclient -n --eval "(progn (x-focus-frame nil)(browse-url-mail \"$@\"))"
-fi
+emacsclient -r -n --eval "(progn (x-focus-frame nil)(browse-url-mail \"$@\"))"
